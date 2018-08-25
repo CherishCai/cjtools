556d1c2a08c06fa74147060ddc10cb350d344653
caihongwen <caihongwen@u51.com>
2018-08-25 19:19:08 +0800

    good to show

--- cjtool.sh
+++ cjtool.sh
@@ -14,35 +14,43 @@ FREE=''
 JSTACK_LOG="/tmp/jstack-${PID}-${DATE}.jstack"
 
 psjava(){
-    PJ=`ps axu | grep -v 'grep' | egrep 'java|PID'`
-    echo -e "\n>>> ps axu | grep -v 'grep' | egrep 'java|PID'"
-    echo -e "$PJ"
+    echo -e "\n\033[34m>>> ps axu | grep -v 'grep' | egrep 'java|PID'\033[0m"
+    ps axu | grep -v 'grep' | egrep 'java|PID'
 
-    if [[ "x" == "x$PJ" ]];then
+    psjavacount=`ps axu | grep -v 'grep' | grep -c java`
+
+    if [ 0 -eq $psjavacount ];then
         echo "has no jvm process, so exit"
         exit -1
     fi
+    
+    PJ=`ps axu | grep -v 'grep' | grep java`
+    
+    if [ 1 -eq $psjavacount ];then
+        PID=`echo $PJ | cut -d ' ' -f 2`
+    fi
+
 }
 
 topPid(){
     TP_LOG=top_Hp-${PID}-${DATE}.log
     TP=`top -n 1 -Hp $PID`
-    echo -e "\n>>> top -Hp $PID"
+    echo -e "\n\033[34m>>> top -Hp $PID\033[0m"
     echo -e "$TP"
-    echo -e "Write (top -Hp $PID) msg into $TP_LOG\n"
+    echo -e "\033[34mWrite (top -Hp $PID) msg into $TP_LOG\033[0m\n"
     echo -e "$TP" > $TP_LOG
 }
 
 jstackPid(){
     JSTACK_LOG=jstack-${PID}-${DATE}.jstack
-    echo -e "$(date '+%Y-%m-%d %H:%M:%S') Begin to process jstack. write into $JSTACK_LOG"
+    echo -e "\033[34m$(date '+%Y-%m-%d %H:%M:%S') Begin to process jstack. write into $JSTACK_LOG\033[0m"
 
     jstack -l $PID > $JSTACK_LOG
     if [[ $? != 0 ]]; then
         echo -e "\033[31mprocess jstack error.\033[0m"
     fi
 
-    echo -e "$(date '+%Y-%m-%d %H:%M:%S') Finish to process jstack.\n"
+    echo -e "\033[34m$(date '+%Y-%m-%d %H:%M:%S') Finish to process jstack.\033[0m\n"
     sleep ${SLEEP_TIME}
 }
 
@@ -50,50 +58,50 @@ freeCmd(){
     FREE_LOG=free_m-${PID}-${DATE}.log
     FREE=`free -m`
     echo ""
-    echo ">>> free -m"
+    echo -e "\033[34m>>> free -m\033[0m"
     echo -e "$FREE"
-    echo -e "Write (free -m) msg into $FREE_LOG\n"
+    echo -e "\033[34mWrite (free -m) msg into $FREE_LOG\033[0m\n"
     echo -e "$FREE" > $FREE_LOG
 }
 
 jinfoFlags(){
     # jinfo -flags $PID
     JINFO_FLAGS_LOG=jinfo_flags-${PID}-${DATE}.log
-    echo -e "\n$(date '+%Y-%m-%d %H:%M:%S') Begin to process jinfo -flags. write into $JINFO_FLAGS_LOG"
+    echo -e "\n\033[34m$(date '+%Y-%m-%d %H:%M:%S') Begin to process jinfo -flags. write into $JINFO_FLAGS_LOG\033[0m"
     jinfo -flags $PID 1>${JINFO_FLAGS_LOG} 2>&1
     if [[ $? != 0 ]]; then
       echo -e "\033[31mprocess jinfo -flags error.\033[0m"
     fi
-    echo -e "$(date '+%Y-%m-%d %H:%M:%S') Finish to process jinfo -flags."
+    echo -e "\033[34m$(date '+%Y-%m-%d %H:%M:%S') Finish to process jinfo -flags.\033[0m"
 }
 
 jmapHeap(){
     #jmap -heap
     JMAP_HEAP_LOG=jmap_heap-${PID}-${DATE}.log
-    echo -e "\n$(date '+%Y-%m-%d %H:%M:%S') Begin to process jmap -heap. write into $JMAP_HEAP_LOG"
+    echo -e "\n\033[34m$(date '+%Y-%m-%d %H:%M:%S') Begin to process jmap -heap. write into $JMAP_HEAP_LOG\033[0m"
     
     jmap -heap $PID > ${JMAP_HEAP_LOG}
     if [[ $? != 0 ]]; then
       echo -e "\033[31mprocess jmap -heap error.\033[0m"
     fi
-    echo -e "$(date '+%Y-%m-%d %H:%M:%S') Finish to process jmap -heap."
+    echo -e "\033[34m$(date '+%Y-%m-%d %H:%M:%S') Finish to process jmap -heap.\033[0m"
 }
 
 jmapHisto(){
     # jmap -histo
     JMAP_HISTO_LOG=jmap_histo-${PID}-${DATE}.log
-    echo -e "$(date '+%Y-%m-%d %H:%M:%S') Begin to process jmap -histo. write into $JMAP_HISTO_LOG"
+    echo -e "\033[34m$(date '+%Y-%m-%d %H:%M:%S') Begin to process jmap -histo. write into $JMAP_HISTO_LOG\033[0m"
     
     jmap -histo $PID > ${JMAP_HISTO_LOG}
     if [[ $? != 0 ]]; then
         echo -e "\033[31mprocess jmap -histo error.\033[0m"
     fi
-    echo -e "$(date '+%Y-%m-%d %H:%M:%S') Finish to process jmap -histo.\n"
+    echo -e "\033[34m$(date '+%Y-%m-%d %H:%M:%S') Finish to process jmap -histo.\033[0m\n"
     sleep ${SLEEP_TIME}
 }
 
 needFullGC(){
-    echo -e "#######\n"
+    echo -e "\033[41;37m###################################\033[0m\n"
     echo -e "\033[31mBelow cmd need full GC, do it if you need!\033[0m"
 
     JMAP_HISTO_LIVE_LOG=jmap_histo_live-${PID}-${DATE}.log
@@ -102,7 +110,7 @@ needFullGC(){
     JMAP_DUMP_FILE=jmap_dump_live-${PID}-${DATE}.bin
     echo "jmap -dump:live,format=b,file=${JMAP_DUMP_FILE} $PID"
 
-    echo -e "\n#######\n"
+    echo -e "\n\033[41;37m###################################\033[0m\n"
 
 }
 
@@ -118,9 +126,12 @@ pidAndChoice(){
     while [[ "x" == "x$PID" ]]; do
         # do ps java
         psjava
-        read -p "Please enter PID:" PID
+        if [[ "x" == "x$PID" ]];then
+            read -p "Please enter PID:" PID
+        fi
     done
 
+    echo -e "\033[35mThe PID is $PID\033[0m"
     echo ""
     echo "1. CPU"
     echo "2. Memory"
@@ -131,15 +142,14 @@ pidAndChoice(){
     if [[ "x" == "x$choice" ]]; then
         choice='9'
     fi
+
+    echo ""
+    echo -e "\033[35mThe choice is $choice\033[0m"
 }
 
 main(){
 
     pidAndChoice
-
-    echo ""
-    echo "PID:$PID"
-    echo "choice:$choice"
 
     if [ 9 -eq $choice -o 8 -eq $choice ]; then
         suggest
